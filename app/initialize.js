c6fdc81278db2fdb11d997c66f57f3b718d3d4ea
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-28 17:53:53 +0200

    Update initialize.js

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,27 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.adball.tech")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
 });
+
